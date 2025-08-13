1923c0125fa017affe847caadc1a33191ec74d1f
Nghia Tran Tuan <nghia_tt@dongabank.com.vn>
2025-08-13 17:18:49 +0700

    initial project

---
+++ src/main/java/com/it/ticket/TicketApplication.java
@@ -0,0 +1,14 @@
+package com.it.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class TicketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TicketApplication.class, args);
+    }
+}
+
